4bbba2d45ec5fd1aafe483c7f6926f648bc9c045
Swati2728 <swati19@navgurukul.org>
2020-09-13 13:36:20 +0530

    my request code

---
+++ logical_question.py
@@ -0,0 +1,85 @@
+# (print max and mini from the list)
+
+# list1=[1,6,7,9]
+# i=0
+# max1=0
+# min1=list1[i]
+# while i<len(list1):
+#     if list1[i]>max1: 
+#         max1=list1[i] 
+#     if list1[i]<min1:
+#         min1=list1[i]
+#     i=i+1
+# print("maximum is",max1)  
+# print("minimum is",min1)
+
+# (mistake was i  was doing )
+
+# list1=[1,6,7,9]
+# i=0
+# max1=0
+# min1=0
+# while i<len(list1):
+#     j=0
+#     while j<len(list1):
+#         if list1[i]>max1: 
+#             max1=list1[i] 
+#         if list1[i]<min1:
+#             min1=list1[i]
+#         j=j+1
+#     i=i+1
+# print("maximum is",max1)  
+# print("minimum is",min1)
+
+# (print the list,index of the list,typ of the list and print the numbers whixh are old and even from the list)
+
+# var=[1,2,3,4,5,6]
+# print(var)
+# print(type(var))
+# print(var[0]) 
+# index=0
+# while index<len(var):
+#     if index%2==0:
+#        print(index)
+#     index=index+1   
+
+# prime_numbers=[1,2,3,4,5,6,7,8,9,10]
+# new_list=[]
+# i=0
+# while i < len(prime_numbers):
+#     j=0
+#     while j<len(prime_numbers):
+#         if prime_numbers[i]
+
+# sum of the list
+
+# student_marks = [23, 45, 89, 90, 56, 80] 
+# i=0
+# sum=0
+# while i<len(student_marks):
+#     sum=sum+student_marks[i]
+#     i=i+1
+# print(sum)    
+
+# ount of the list which are more and less than the 50
+
+# student_marks = [23, 45, 67, 89, 90, 54, 34, 21, 34, 23, 19, 28, 10, 45, 86, 87, 9]
+# index=0
+# more_than=0
+# less_than=0
+# count1=0
+# count2=0
+# while index<len(student_marks):
+#     if student_marks[index]<50:
+#         less_than=less_than+1    
+#         count1+=1
+#     if student_marks[index]>50:
+#         more_than=more_than+1
+#         count2+=1
+#     index=index+1
+# print("total less than 50 are:",count1)    
+# print("total more than 50 are:",count2)
+
+
+
+
